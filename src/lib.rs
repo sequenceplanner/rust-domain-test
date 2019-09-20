685f0e3490325a783b1d60bb4c0ae045de91f357
Kristofer Bengtsson <kristofer@sekvensa.se>
2019-09-20 12:59:38 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 //! The SP domain
 
-#![allow(unused_mut)]  // for some reason I get not a correct warning for mut in macros
-
-
+#![allow(unused_mut)] // for some reason I get not a correct warning for mut in macros
 
 mod values;
 pub use values::*;
@@ -13,11 +11,10 @@ pub use predicates::*;
 mod states;
 pub use states::*;
 
-
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::error;
 use std::fmt;
-use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum SPItem<'a> {
@@ -36,7 +33,7 @@ pub enum SPItem<'a> {
 pub struct SPNode<'a> {
     name: String,
     #[serde(skip)]
-    parent: Option<&'a SPItem<'a>>
+    parent: Option<&'a SPItem<'a>>,
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
@@ -44,8 +41,8 @@ pub struct Resource<'a> {
     node: SPNode<'a>,
     abilities: Vec<Ability<'a>>,
     parameters: Vec<Variable<'a>>,
-    messages: Vec<Topic<'a>>,  // Also include estimated here on an estimated topic
-    //pub comm: ResourceComm,
+    messages: Vec<Topic<'a>>, // Also include estimated here on an estimated topic
+                              //pub comm: ResourceComm,
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
@@ -57,13 +54,13 @@ pub struct Topic<'a> {
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
 pub struct Message<'a> {
     node: SPNode<'a>,
-    fields: Vec<(String, MessageFields<'a>)>
+    fields: Vec<(String, MessageFields<'a>)>,
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum MessageFields<'a> {
     Msg(Message<'a>),
-    Var(Variable<'a>)
+    Var(Variable<'a>),
 }
 
 impl<'a> Default for MessageFields<'a> {
@@ -87,7 +84,7 @@ pub enum VariableType {
     Measured,
     Estimated,
     Command,
-    Parameter(Option<SPPath>)
+    Parameter(Option<SPPath>),
 }
 
 impl<'a> Default for VariableType {
@@ -95,7 +92,6 @@ impl<'a> Default for VariableType {
         VariableType::Estimated
     }
 }
-
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
 pub struct Transition<'a> {
@@ -105,16 +101,10 @@ pub struct Transition<'a> {
     effects: Vec<Action>,
 }
 
-
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
 pub struct Ability<'a> {
     node: SPNode<'a>,
-
 }
-
-
-
-
 
 /// The SPPath is used for identifying all objects in a model. The path will be defined
 /// based on where the item is in the model hierarchy
@@ -122,7 +112,7 @@ pub struct Ability<'a> {
 pub enum SPPath {
     LocalPath(Vec<String>),
     GlobalPath(Vec<String>),
-    NoPath
+    NoPath,
 }
 
 impl Default for SPPath {
@@ -134,17 +124,17 @@ impl Default for SPPath {
 impl std::fmt::Display for SPPath {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SPPath::LocalPath(xs) => write!(f,"L:{}", xs.join("/")),
-            SPPath::GlobalPath(xs) => write!(f,"G:{}", xs.join("/")),
-            SPPath::NoPath => write!(f,"NOPATH"),
+            SPPath::LocalPath(xs) => write!(f, "L:{}", xs.join("/")),
+            SPPath::GlobalPath(xs) => write!(f, "G:{}", xs.join("/")),
+            SPPath::NoPath => write!(f, "NOPATH"),
         }
     }
 }
 
-
-
 impl SPPath {
-    pub fn new() -> SPPath { SPPath::NoPath }
+    pub fn new() -> SPPath {
+        SPPath::NoPath
+    }
     pub fn from(xs: &[String]) -> SPPath {
         let v: Vec<String> = xs.iter().map(|s| s.to_string()).collect();
         SPPath::LocalPath(v)
@@ -155,24 +145,22 @@ impl SPPath {
     }
     pub fn from_string(s: &str) -> Result<SPPath> {
         if s == "NOPATH" {
-            return Ok(SPPath::NoPath)
+            return Ok(SPPath::NoPath);
         }
         let what_type: Vec<&str> = s.split(":").collect();
 
         match what_type.as_slice() {
-            ["NOPATH"]  => return Ok(SPPath::NoPath),
+            ["NOPATH"] => return Ok(SPPath::NoPath),
             ["L", tail] => {
-                let res: Vec<&str> = tail.split("/").collect(); 
-                return Ok(SPPath::from_array(&res))
-            },
+                let res: Vec<&str> = tail.split("/").collect();
+                return Ok(SPPath::from_array(&res));
+            }
             ["G", tail] if tail != &"" => {
-                let res: Vec<&str> = tail.split("/").filter(|x| !x.is_empty()).collect(); 
-                return Ok(SPPath::from_array_to_global(&res))
-            },
-            _ => return Err(SPError::No(format!("Can not convert {} into a path", s)))
-
+                let res: Vec<&str> = tail.split("/").filter(|x| !x.is_empty()).collect();
+                return Ok(SPPath::from_array_to_global(&res));
+            }
+            _ => return Err(SPError::No(format!("Can not convert {} into a path", s))),
         }
-
     }
     pub fn from_to_global(n: &[String]) -> SPPath {
         let v: Vec<String> = n.iter().map(|s| s.to_string()).collect();
@@ -186,25 +174,26 @@ impl SPPath {
         match self {
             SPPath::LocalPath(xs) => xs.clone(),
             SPPath::GlobalPath(xs) => xs.clone(),
-            SPPath::NoPath => vec!()
+            SPPath::NoPath => vec![],
         }
     }
     pub fn string_path(&self) -> String {
         format!("{}", self)
     }
     pub fn is_child_of(&self, other: &SPPath) -> bool {
-        (self.path().len() >= other.path().len()) && other.path().iter().zip(self.path().iter()).all(|(a, b)| a == b)
+        (self.path().len() >= other.path().len())
+            && other
+                .path()
+                .iter()
+                .zip(self.path().iter())
+                .all(|(a, b)| a == b)
     }
 }
-
-
-
-
 
 type Result<T> = std::result::Result<T, SPError>;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
-pub enum SPError{
+pub enum SPError {
     OverwriteDelay(states::Delay, AssignStateValue),
     OverwriteNext(states::Next, AssignStateValue),
     No(String),
@@ -214,18 +203,18 @@ pub enum SPError{
 impl fmt::Display for SPError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            SPError::OverwriteDelay(next, prev) => {
-                write!(f, "You are trying to overwrite a Delay in the State. current: {:?}, new: {:?} ", prev, next)
-            },
-            SPError::OverwriteNext(next, prev) => {
-                write!(f, "You are trying to overwrite a Next in the State. current: {:?}, new: {:?} ", prev, next)
-            },
-            SPError::Undefined  => {
-                write!(f, "An undefined SP error!")
-            },
-            SPError::No(s)  => {
-                write!(f, "Oh No: {}", s)
-            },
+            SPError::OverwriteDelay(next, prev) => write!(
+                f,
+                "You are trying to overwrite a Delay in the State. current: {:?}, new: {:?} ",
+                prev, next
+            ),
+            SPError::OverwriteNext(next, prev) => write!(
+                f,
+                "You are trying to overwrite a Next in the State. current: {:?}, new: {:?} ",
+                prev, next
+            ),
+            SPError::Undefined => write!(f, "An undefined SP error!"),
+            SPError::No(s) => write!(f, "Oh No: {}", s),
         }
     }
 }
@@ -235,9 +224,6 @@ impl error::Error for SPError {
         None
     }
 }
-
-
-
 
 #[cfg(test)]
 mod tests_paths {
@@ -254,14 +240,15 @@ mod tests_paths {
         assert_eq!(SPPath::from_string("L:a/b"), Ok(l_ab.clone()));
         assert_eq!(SPPath::from_string("NOPATH"), Ok(SPPath::NoPath));
         assert_eq!(SPPath::from_string("NOPATH"), Ok(SPPath::NoPath));
-        
+
         assert!(SPPath::from_string("G:").is_err());
         assert!(SPPath::from_string("G/no").is_err());
         assert!(SPPath::from_string("H:a/b/").is_err());
         assert!(SPPath::from_string("a/b/").is_err());
         assert!(SPPath::from_string("G:top_path").is_ok());
-        assert_eq!(SPPath::from_string("G:a/b//k/"), 
-                   Ok(SPPath::from_array_to_global(&["a", "b", "k"])));
-        
+        assert_eq!(
+            SPPath::from_string("G:a/b//k/"),
+            Ok(SPPath::from_array_to_global(&["a", "b", "k"]))
+        );
     }
 }
