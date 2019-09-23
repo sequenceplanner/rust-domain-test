2fd807fb5b2bea2ca953a46767aa56eab41c9e8e
Kristofer Bengtsson <kristofer@sekvensa.se>
2019-09-23 14:39:43 +0200

    Updated domain to use a SPPath in the nodes instead of a pointer.

--- src/lib.rs
+++ src/lib.rs
@@ -18,19 +18,19 @@ use std::fmt;
 
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-pub enum SPItem<'a> {
-    Model(Model<'a>),
-    Resource(Resource<'a>),
-    Message(Message<'a>),
-    Topic(Topic<'a>),
-    Variable(Variable<'a>),
-    Operation(Operation<'a>),
-    Ability(Ability<'a>),
-    Transition(Transition<'a>),
+pub enum SPItem {
+    Model(Model),
+    Resource(Resource),
+    Message(Message),
+    Topic(Topic),
+    Variable(Variable),
+    Operation(Operation),
+    Ability(Ability),
+    Transition(Transition),
     //SOP(SOP),
 }
 
-impl<'a> SPItem<'a> {
+impl SPItem {
     pub fn name(&self) -> &str {
         &self.node().name
     }
@@ -46,108 +46,258 @@ impl<'a> SPItem<'a> {
             SPItem::Transition(x) => &x.node,
         }
     }
-    fn node_mut(&'a mut self) -> &'a mut SPNode {
+    // fn node_mut(&mut self) -> &mut SPNode {
+    //     match self {
+    //         SPItem::Model(x) => &mut x.node,
+    //         SPItem::Resource(x) => &mut x.node,
+    //         SPItem::Message(x) => &mut x.node,
+    //         SPItem::Topic(x) => &mut x.node,
+    //         SPItem::Variable(x) => &mut x.node,
+    //         SPItem::Operation(x) => &mut x.node,
+    //         SPItem::Ability(x) => &mut x.node,
+    //         SPItem::Transition(x) => &mut x.node,
+    //     }
+    // }
+    pub fn update_path(&mut self, mut local: SPPath, mut global: SPPath) -> (SPPath, SPPath) {
         match self {
-            SPItem::Model(x) => &mut x.node,
-            SPItem::Resource(x) => &mut x.node,
-            SPItem::Message(x) => &mut x.node,
-            SPItem::Topic(x) => &mut x.node,
-            SPItem::Variable(x) => &mut x.node,
-            SPItem::Operation(x) => &mut x.node,
-            SPItem::Ability(x) => &mut x.node,
-            SPItem::Transition(x) => &mut x.node,
+            SPItem::Model(x) => x.update_path(local, global),
+            SPItem::Resource(x) => x.update_path(local, global),
+            SPItem::Message(x) => x.update_path(local, global),
+            SPItem::Topic(x) => x.update_path(local, global),
+            SPItem::Variable(x) => x.update_path(local, global),
+            SPItem::Operation(x) => x.update_path(local, global),
+            SPItem::Ability(x) => x.update_path(local, global),
+            SPItem::Transition(x) => x.update_path(local, global),
         }
-    }
-    pub fn upd_parent(&'a mut self, parent: &'a SPItem) {
-        self.node_mut().upd_parent(parent)
+
     }
 }
 
-#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-pub struct SPNode<'a> {
+#[derive(PartialEq, Clone, Default, Serialize, Deserialize)]
+pub struct SPNode {
     name: String,
-    #[serde(skip)]
-    parent: Option<&'a SPItem<'a>>,
+    local_path: SPPath,
+    global_path: SPPath,
 }
 
-impl<'a> SPNode<'a> {
-    pub fn new(name: &str) -> SPNode<'a> {
+impl SPNode {
+    pub fn new(name: &str) -> SPNode {
         SPNode {
             name: name.to_string(),
-            parent: None,
+            local_path: SPPath::NoPath,
+            global_path: SPPath::NoPath,
         }
     }
 
-    pub fn parent(&self) -> Option<&'a SPItem> {
-        self.parent.clone()
+    pub fn local_path(&self) -> SPPath {
+        self.local_path.clone()
+    }
+    pub fn global_path(&self) -> SPPath {
+        self.global_path.clone()
     }
 
-    pub fn upd_parent(&mut self, parent: &'a SPItem) {
-        self.parent = Some(parent);
-    }
-}
-
-// Never clone the parent since we do not want to point to another model
-impl Clone for SPNode<'_> {
-    fn clone(&self) -> Self {
-        SPNode::new(&self.name)
-    }
-}
-
-impl std::fmt::Display for SPNode<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.parent() {
-            Some(p) => write!(f, "name:{}, p({})", self.name, p.name()),
-            None => write!(f, "name:{}", self.name),
+    pub fn update_path(&mut self, mut local: SPPath, mut global: SPPath) -> (SPPath, SPPath) {
+        local.add(self.name.clone());
+        global.add(self.name.clone());
+        if let SPPath::GlobalPath(_) = local {
+            panic!("Must assign local, not {}", local);
         }
+        if let SPPath::LocalPath(_) = global {
+            panic!("Must assign global, not {}", global);
+        }
+        self.local_path = local;
+        self.global_path = global;
+        (self.local_path(), self.global_path())
+    }
+
+    fn write_nice(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut res = String::new();
+        res.push_str(&format!("{}- ", self.name));
+        if let SPPath::LocalPath(_) = self.local_path {
+            res.push_str(&format!("{}, ", self.local_path));
+        }
+        if let SPPath::GlobalPath(_) = self.global_path {
+            res.push_str(&format!("{}, ", self.global_path));
+        }
+        write!(f, "{}", res)
+    }
+}
+
+
+impl std::fmt::Display for SPNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.write_nice(f)
+    }
+}
+
+impl std::fmt::Debug for SPNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.write_nice(f)
     }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Model<'a> {
-    node: SPNode<'a>,
-    items: Vec<SPItem<'a>>
+pub struct Model {
+    node: SPNode,
+    items: Vec<SPItem>
+}
+
+impl Model {
+    pub fn new(name: &str, items: Vec<SPItem>) -> Model {
+        let node = SPNode::new(name);
+        Model {
+            node,
+            items
+        }
+    }
+
+    pub fn items(&self) -> &[SPItem] {
+        self.items.as_slice()
+    }
+
+    pub fn add_item(&mut self, mut item: SPItem) {
+        item.update_path(self.node.local_path(), self.node.global_path());
+        self.items.push(item);
+    }
+
+    pub fn update_path(&mut self, mut local: SPPath, mut global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+        self.items.iter_mut().for_each(|i| {
+            i.update_path(local.clone(), global.clone());
+        });
+        (local, global)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Resource<'a> {
-    node: SPNode<'a>,
-    abilities: Vec<Ability<'a>>,
-    parameters: Vec<Variable<'a>>,
-    messages: Vec<Topic<'a>>, // Also include estimated here on an estimated topic
+pub struct Resource {
+    node: SPNode,
+    abilities: Vec<Ability>,
+    parameters: Vec<Variable>,
+    messages: Vec<Topic>, // Also include estimated here on an estimated topic
                               //pub comm: ResourceComm,
 }
 
+impl Resource {
+    
+    pub fn new(name: &str) -> Resource {
+        let node = SPNode::new(name);
+        Resource {
+            node,
+            ..Resource::default()
+        }
+    }
+    
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+
+        self.abilities.iter_mut().for_each(|i| {
+            i.update_path(local.clone(), global.clone());
+        });
+        self.parameters.iter_mut().for_each(|i| {
+            i.update_path(local.clone(), global.clone());
+        });
+        self.messages.iter_mut().for_each(|i| {
+            i.update_path(local.clone(), global.clone());
+        });
+        (local, global)
+    }
+}
+
+
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Topic<'a> {
-    node: SPNode<'a>,
-    msg: Message<'a>,
+pub struct Topic {
+    node: SPNode,
+    msg: Message,
+}
+
+impl Topic {
+    pub fn new(name: &str, msg: Message) -> Topic {
+        let node = SPNode::new(name);
+        Topic {
+            node,
+            msg
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+        self.msg.update_path(local.clone(), global.clone());
+        (local, global)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Message<'a> {
-    node: SPNode<'a>,
-    fields: Vec<(String, MessageFields<'a>)>,
+pub struct Message {
+    node: SPNode,
+    fields: Vec<(String, MessageFields)>,
+}
+
+impl Message {
+    pub fn new(name: &str) -> Message {
+        let node = SPNode::new(name);
+        Message {
+            node,
+            fields: vec!()
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+        self.fields.iter_mut().for_each(|(_, value)| {
+            value.update_path(local.clone(), global.clone());
+        });
+        (local, global)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-pub enum MessageFields<'a> {
-    Msg(Message<'a>),
-    Var(Variable<'a>),
+pub enum MessageFields {
+    Msg(Message),
+    Var(Variable),
 }
 
-impl<'a> Default for MessageFields<'a> {
+impl MessageFields {
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        match self {
+            MessageFields::Msg(ref mut x) => x.update_path(local, global),
+            MessageFields::Var(ref mut x) => x.update_path(local, global)
+        }
+    }
+}
+
+impl Default for MessageFields {
     fn default() -> Self {
         MessageFields::Var(Variable::default())
     }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Variable<'a> {
-    node: SPNode<'a>,
-    type_: VariableType,
+pub struct Variable {
+    node: SPNode,
+    pub type_: VariableType,
     pub initial_value: Option<SPValue>,
     pub domain: Vec<SPValue>,
+}
+
+impl Variable {
+    pub fn new(name: &str, 
+                type_: VariableType,
+                initial_value: Option<SPValue>,
+                domain: Vec<SPValue>,
+                ) -> Variable {
+        let node = SPNode::new(name);
+        Variable {
+            node,
+            type_,
+            initial_value,
+            domain,
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        self.node.update_path(local, global)
+    }
 }
 
 /// The possible variable types used by operations to define parameters
@@ -161,48 +311,163 @@ pub enum VariableType {
     Predicate(Predicate)
 }
 
-impl<'a> Default for VariableType {
+impl Default for VariableType {
     fn default() -> Self {
         VariableType::Estimated
     }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Transition<'a> {
-    node: SPNode<'a>,
+pub struct Transition {
+    node: SPNode,
     guard: Predicate,
     actions: Vec<Action>,
     effects: Vec<Action>,
 }
 
-#[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Ability<'a> {
-    node: SPNode<'a>,
-    controlled: Vec<Transition<'a>>,
-    uncontrolled: Vec<Transition<'a>>,
-    predicates: Vec<Variable<'a>>
+impl Transition {
+    pub fn new(name: &str, 
+               guard: Predicate,
+               actions: Vec<Action>,
+               effects: Vec<Action> ) -> Transition {
+        let node = SPNode::new(name);
+        Transition {
+            node,
+            guard,
+            actions,
+            effects
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        self.node.update_path(local, global)
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct Operation<'a> {
-    node: SPNode<'a>,
-    precondition: Vec<Transition<'a>>,
-    postcondition: Vec<Transition<'a>>,
-    uncontrolled: Vec<Transition<'a>>,
-    predicates: Vec<Variable<'a>>,
-    goal: IfThen<'a>,
-    invariant: IfThen<'a>,
+pub struct Ability {
+    node: SPNode,
+    controlled: Vec<Transition>,
+    uncontrolled: Vec<Transition>,
+    predicates: Vec<Variable>
+}
 
+impl Ability {
+    pub fn new(name: &str,
+                controlled: Vec<Transition>,
+                uncontrolled: Vec<Transition>,
+                predicates: Vec<Variable>
+    ) -> Ability {
+        let node = SPNode::new(name);
+        Ability {
+            node,
+            controlled,
+            uncontrolled,
+            predicates
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+        self.controlled.iter_mut().for_each(|t| {
+            t.update_path(local.clone(), global.clone());
+        });
+        self.uncontrolled.iter_mut().for_each(|t| {
+            t.update_path(local.clone(), global.clone());
+        });
+        self.predicates.iter_mut().for_each(|v| {
+            v.update_path(local.clone(), global.clone());
+        });
+
+
+        (local, global)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
+pub struct Operation {
+    node: SPNode,
+    precondition: Vec<Transition>,
+    postcondition: Vec<Transition>,
+    uncontrolled: Vec<Transition>,
+    predicates: Vec<Variable>,
+    goal: Option<IfThen>,
+    invariant: Option<IfThen>,
+}
+
+impl Operation {
+    pub fn new( name: &str,
+                precondition: Vec<Transition>,
+                postcondition: Vec<Transition>,
+                uncontrolled: Vec<Transition>,
+                predicates: Vec<Variable>,
+                goal: Option<IfThen>,
+                invariant: Option<IfThen>
+    ) -> Operation {
+        let node = SPNode::new(name);
+        Operation {
+            node,
+            precondition,
+            postcondition,
+            uncontrolled,
+            predicates,
+            goal,
+            invariant,
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        let (local, global) = self.node.update_path(local, global);
+        self.precondition.iter_mut().for_each(|t| {
+            t.update_path(local.clone(), global.clone());
+        });
+        self.postcondition.iter_mut().for_each(|t| {
+            t.update_path(local.clone(), global.clone());
+        });
+        self.uncontrolled.iter_mut().for_each(|t| {
+            t.update_path(local.clone(), global.clone());
+        });
+        self.predicates.iter_mut().for_each(|v| {
+            v.update_path(local.clone(), global.clone());
+        });
+        if let Some(ref mut x) = self.goal {
+            x.update_path(local.clone(), global.clone());
+        }
+        if let Some(ref mut x) = self.invariant {
+            x.update_path(local.clone(), global.clone());
+        }
+
+
+        (local, global)
+    }
 }
 
 /// An IfThen is used by operaitons to define goals or invariants. When the if_
 /// predicate is true, then the then_ predicate is either a goal or an invariant
 /// that the planner will use for planning. TODO: Maybe we should have a better name?
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
-pub struct IfThen<'a> {
-    node: SPNode<'a>,
+pub struct IfThen {
+    node: SPNode,
     if_: Predicate,
     then_: Predicate,
+}
+
+impl IfThen {
+    pub fn new( name: &str,
+                if_: Predicate,
+                then_: Predicate
+    ) -> IfThen {
+        let node = SPNode::new(name);
+        IfThen {
+            node,
+            if_,
+            then_
+        }
+    }
+
+    pub fn update_path(&mut self, local: SPPath, global: SPPath) -> (SPPath, SPPath) {
+        self.node.update_path(local, global)
+    }
 }
 
 
@@ -223,7 +488,7 @@ impl Default for SPPath {
 }
 
 impl std::fmt::Display for SPPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             SPPath::LocalPath(xs) => write!(f, "L:{}", xs.join("/")),
             SPPath::GlobalPath(xs) => write!(f, "G:{}", xs.join("/")),
@@ -235,6 +500,16 @@ impl std::fmt::Display for SPPath {
 impl SPPath {
     pub fn new() -> SPPath {
         SPPath::NoPath
+    }
+    pub fn new_local() -> SPPath {
+        SPPath::LocalPath(vec!())
+    }
+    pub fn add(&mut self, name: String) {
+        match self {
+            SPPath::LocalPath(ref mut xs) => xs.push(name),
+            SPPath::GlobalPath(ref mut xs) => xs.push(name),
+            SPPath::NoPath => println!("Tried to push {} to an NoPath", name)
+        }
     }
     pub fn from(xs: &[String]) -> SPPath {
         let v: Vec<String> = xs.iter().map(|s| s.to_string()).collect();
@@ -332,25 +607,23 @@ mod tests_domain {
     use super::*;
     #[test]
     fn making() {
-        // let mut m = SPItem::Model(Model{
-        //         node: SPNode::new("m"),
-        //         items: vec!()
-        //     });
-        // let n = SPItem::Model(Model{
-        //     node: SPNode::new("n"),
-        //     items: vec!()
-        // });
+        let mut m = SPItem::Model(Model{
+                node: SPNode::new("m"),
+                items: vec!()
+            });
 
-        // if let SPItem::Model(mut my) = m {
-        //     my.items.push(n);
-        //     my.node.upd_parent(parent: &'a SPItem)
-        // }
-        // m.upd_parent(&n);
-        // m
-
-        // let v = x();
+        m.update_path(SPPath::new_local(), SPPath::from_array_to_global(&["a", "b"]));
         
-        // println!("{:?}", v);
+        let n = SPItem::Model(Model{
+            node: SPNode::new("n"),
+            items: vec!()
+        });
+
+        if let SPItem::Model(ref mut my) = m {
+            my.add_item(n);
+        }
+        
+        println!("{:?}", m);
     }
 }
 
